29f6993509a89f6f2ec436942244a7db2497630e
Mike Lubinets <public@mersinvald.me>
2019-03-06 12:13:38 +0100

    ensure svm build in build.sh, disable gocache as it causes problems

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+pushd $GOPATH/src/github.com/etclabscore/sputnikvm-ffi/c/
+make debug
+popd
+
 mkdir -p build/bin
-CGO_LDFLAGS="$GOPATH/src/github.com/etclabscore/sputnikvm-ffi/c/libsputnikvm.a -ldl -lm" go build -o build/bin/geth ./cmd/geth
+GOCACHE=off CGO_LDFLAGS="$GOPATH/src/github.com/etclabscore/sputnikvm-ffi/c/libsputnikvm.a -ldl -lm" go build -o build/bin/geth ./cmd/geth
 
